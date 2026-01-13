dc66343ac48d59766114ae029c5e9e8b23f819e2
Stanley Morales <abrahamx.carranza@gmail.com>
2026-01-13 13:06:53 +0100

    del: remove files

--- apps/client/src/test-zwsp.js
+++
@@ -1,6 +0,0 @@
-
-const zwsp = "\u200B";
-const text = "A";
-console.log(`Length of ZWSP: ${zwsp.length}`);
-console.log(`Trimmed length of ZWSP: ${zwsp.trim().length}`);
-console.log(`Is ZWSP visible (regex \\S): ${/\S/.test(zwsp)}`);
